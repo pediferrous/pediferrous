d7c40e0f45591048569392cef759221df1ea7fd3
Nadir Fejzic <nadir@notfloor.com>
2024-11-30 22:46:25 +0100

    feat: implement indirect object reference data type

--- pdfgen/src/types/hierarchy/primitives/mod.rs
+++ pdfgen/src/types/hierarchy/primitives/mod.rs
@@ -1,2 +1,3 @@
 pub mod name;
+pub mod obj_ref;
 pub mod rectangle;
---
+++ pdfgen/src/types/hierarchy/primitives/obj_ref.rs
@@ -0,0 +1,34 @@
+//! Implementation of PDF object reference.
+
+use std::io::{Error, Write};
+
+use crate::types;
+
+/// Any object in a PDF file may be labelled as an indirect object. This gives the object a unique
+/// object identifier by which other objects can refer to it. The object may be referred to from
+/// elsewhere in the file by an indirect reference. Such indirect references shall consist of the
+/// object number, the generation number, and the keyword R (with whitespace separating each part).
+///
+/// Example: `4 0 R`
+pub struct ObjRef {
+    /// Identifier of referenced object.
+    id: u64,
+}
+
+impl From<u64> for ObjRef {
+    fn from(id: u64) -> Self {
+        Self { id }
+    }
+}
+
+impl ObjRef {
+    /// Write the encoded PDF object reference into the provided implementor of [`Write`].
+    pub fn write(&self, writer: &mut impl Write) -> Result<usize, Error> {
+        let written = types::write_chain! {
+            writer.write(self.id.to_string().as_bytes()),
+            writer.write(b" 0 R"),
+        };
+
+        Ok(written)
+    }
+}
