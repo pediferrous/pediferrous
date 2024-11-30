bdddf8dbfb93738303fe26d6330b0d3b54c90639
Nadir Fejzic <nadir@notfloor.com>
2024-11-30 16:10:35 +0100

    test: add basic test for `Page` object

--- pdfgen/src/types/hierarchy/page.rs
+++ pdfgen/src/types/hierarchy/page.rs
@@ -15,6 +15,14 @@ impl Page {
     const PARENT: Name = Name::new(b"Parent");
     const RESOURCES: Name = Name::new(b"Resources");
     const MEDIA_BOX: Name = Name::new(b"MediaBox");
+
+    pub fn new(parent: impl Into<String>, media_box: impl Into<String>) -> Self {
+        Self {
+            parent: parent.into(),
+            resources: String::from("<< >>"),
+            media_box: media_box.into(),
+        }
+    }
 }
 
 impl Page {
@@ -23,16 +31,40 @@ impl Page {
             writer.write(b"<< "),
             Name::TYPE.write(writer),
             Self::TYPE.write(writer),
-            writer.write(b"\n"),
+
             Self::PARENT.write(writer),
             writer.write(self.parent.as_bytes()),
+            writer.write(b" "),
+
             Self::RESOURCES.write(writer),
             writer.write(self.resources.as_bytes()),
+            writer.write(b" "),
+
             Self::MEDIA_BOX.write(writer),
             writer.write(self.media_box.as_bytes()),
             writer.write(b" >>"),
         };
 
         Ok(written)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Page;
+
+    #[test]
+    fn basic_page() {
+        let page = Page::new("0 0 R", "[0 0 100 100]");
+
+        let mut writer = Vec::new();
+        page.write(&mut writer).unwrap();
+
+        let output = String::from_utf8(writer).unwrap();
+
+        insta::assert_snapshot!(
+            output,
+            @"<< /Type /Page /Parent 0 0 R /Resources << >> /MediaBox [0 0 100 100] >>"
+        );
     }
 }
