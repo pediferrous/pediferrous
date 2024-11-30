1ee38961b77524f82f33797ffc7a371a07dcaadb
Nadir Fejzic <nadir@notfloor.com>
2024-11-30 15:55:20 +0100

    feat: implement macro for easier write chains

--- pdfgen/src/types/mod.rs
+++ pdfgen/src/types/mod.rs
@@ -6,3 +6,15 @@ pub trait WriteDictValue {
     #[allow(dead_code)]
     fn write(&self, writer: &mut impl Write) -> Result<usize, Error>;
 }
+
+macro_rules! write_chain {
+    ($($expression:expr),* $(,)?) => {{
+        let mut written = 0;
+        $(
+            written += $expression?;
+        )*
+        written
+    }}
+}
+
+pub(crate) use write_chain;
