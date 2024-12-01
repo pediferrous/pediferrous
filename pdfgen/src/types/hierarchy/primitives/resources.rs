f721d5111831a97f7c1d3b69707f23c4197e2e3e
Nadir Fejzic <nadir@notfloor.com>
2024-12-01 21:53:45 +0100

    docs: add documentation for the resource dictionary

--- pdfgen/src/types/hierarchy/primitives/resources.rs
+++ pdfgen/src/types/hierarchy/primitives/resources.rs
@@ -4,21 +4,31 @@ use std::io::{Error, Write};
 
 use crate::types;
 
+/// Represents a single entry in the [`Resources`] dictionary.
 #[derive(Debug, Clone)]
 pub(crate) enum ResourceEntry {}
 
 impl ResourceEntry {
+    /// Encode and write this entry into the implementor of [`Write`].
     fn write(&self, _writer: &mut impl Write) -> Result<usize, Error> {
         Ok(0)
     }
 }
 
+/// Resource dictionary enumerates the named resources needed by the operators in the content
+/// stream and the names by which they can be referred to.
+///
+/// This is used in cases, where an operator shall refer to a PDF object that is defined outside
+/// the content stream, such as a font dictionary or a stream containing image data. This shall be
+/// accomplished by defining such objects as named resources and referring to them by name from
+/// within the content stream.
 #[derive(Default, Debug, Clone)]
 pub struct Resources {
     entries: Vec<ResourceEntry>,
 }
 
 impl Resources {
+    /// Encode and write this resource dictionary into the provided implementor of [`Write`].
     pub(crate) fn write(&self, writer: &mut impl Write) -> Result<usize, Error> {
         let written = types::write_chain! {
             writer.write(b"<< "),
