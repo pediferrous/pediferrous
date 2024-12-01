bd6593bf19b478f55fe8f543955f098245cd0528
Nadir Fejzic <nadir@notfloor.com>
2024-12-01 21:40:47 +0100

    docs: document the primitive types module

--- pdfgen/src/types/hierarchy/primitives/mod.rs
+++ pdfgen/src/types/hierarchy/primitives/mod.rs
@@ -1,3 +1,6 @@
+//! Definition and implementation of primitive PDF data types. These types are mostly used as
+//! values for various entries in object dictionaries (such as Page Tree and Page).
+
 pub mod name;
 pub mod obj_ref;
 pub mod rectangle;
