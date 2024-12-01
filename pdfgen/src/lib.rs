deb87c829958de638b14f5286facbe06a47a93db
Kasim Fejzic <kasimfejzo@gmail.com>
2024-12-01 02:22:30 +0100

    feat: implement an initial version of CRT with PdfWriter

--- pdfgen/src/lib.rs
+++ pdfgen/src/lib.rs
@@ -7,31 +7,89 @@ use std::io::{self, Write};
 
 pub mod types;
 
-/// This represents one cohesive PDF document that can contain multiple pages of content.
-#[derive(Default)]
-pub struct Document;
+/// Comment
+pub trait Object {
+    /// Comment
+    fn write(&self, writer: &mut impl Write) -> Result<usize, io::Error>;
+}
 
-impl Document {
+/// A wrapper around any type that implements `Write`, adding pdf specific functionality to keep a
+/// clear and consistent CrossReferenceTable state
+pub struct PdfWriter<W: Write> {
+    /// Inner member, representing a type that implements `Write`.
+    inner: W,
+    /// Current byte offset from the top of document, representing the current position of the `cursor`.
+    current_offset: usize,
+}
+
+impl<W: Write> PdfWriter<W> {
     /// The PDF file begins with the 5 characters “%PDF–X.X” and byte offsets shall be calculated
     /// from the PERCENT SIGN.
     const PDF_HEADER: &[u8] = b"%PDF-2.0";
     /// The last line of the file shall contain only the end-of-file marker, %%EOF
     const EOF_MARKER: &[u8] = b"%%EOF";
 
-    /// Write the PDF header and return number of written bytes.
-    fn write_header(&self, writer: &mut impl Write) -> Result<usize, io::Error> {
-        writer.write(Self::PDF_HEADER)
+    /// Creates a new `PdfWriter` instance.
+    pub fn new(inner: W) -> Self {
+        PdfWriter {
+            inner,
+            current_offset: 0,
+        }
+    }
+
+    /// Comment
+    pub fn write_header(&mut self) -> Result<(), io::Error> {
+        // Delegate the actual writing to the inner writer incrementing the current_offset to
+        // reflect current 'cursor' position.
+        self.current_offset += self.inner.write(Self::PDF_HEADER)?;
+
+        Ok(())
+    }
+
+    /// Comment
+    pub fn write_object(&mut self, obj: &impl Object) -> Result<(), io::Error> {
+        // Delegate the actual writing to the inner writer.
+        let written = obj.write(&mut self.inner)?;
+
+        Ok(())
     }
 
     /// Write the PDF documents EOF.
-    fn write_eof(&self, writer: &mut impl Write) -> Result<(), io::Error> {
-        writer.write_all(Self::EOF_MARKER)
+    pub fn write_eof(&mut self) -> Result<(), io::Error> {
+        // Delegate the actual writing to the inner writer.
+        self.inner.write_all(Self::EOF_MARKER)
     }
+}
 
+/// This represents the PDF-s cross-reference (xref) table, which is a crucial component that
+/// maps each object in the PDF to its location within the file (byte offset from the start).
+#[derive(Default)]
+pub struct CrossReferenceTable {
+    /// Storing solely byte offsets, since we are considering the generation
+    /// number to be '00000' and in use flag to be 'n' at all times.
+    offsets: Vec<usize>,
+}
+
+impl CrossReferenceTable {
+    /// Marker representing the start of CRT section (4 characters “xref”).
+    const XREF_MARKER: &[u8] = b"xref";
+
+    /// Adds a new object offset to the table.
+    fn add_object(&mut self, byte_offset: usize) {
+        self.offsets.push(byte_offset);
+    }
+}
+
+/// This represents one cohesive PDF document that can contain multiple pages of content.
+#[derive(Default)]
+pub struct Document;
+
+impl Document {
     /// Write the PDF contents into the provided writer.
     pub fn write(&self, writer: &mut impl Write) -> Result<(), io::Error> {
-        let _bytes_written = self.write_header(writer)?;
-        self.write_eof(writer)?;
+        let mut pdf_writer = PdfWriter::new(writer);
+        pdf_writer.write_header()?;
+        pdf_writer.write_eof()?;
 
         Ok(())
     }
