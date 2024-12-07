2b146ef7bf8a04f210b66e054afd9902eb9dffae
Kasim Fejzic <kasimfejzo@gmail.com>
2024-12-07 19:34:06 +0100

    chore: rename `WriteTrailer` extension traits method to `write_trailer`

--- pdfgen/src/types/hierarchy/trailer.rs
+++ pdfgen/src/types/hierarchy/trailer.rs
@@ -12,25 +12,25 @@ use super::{
 /// Comment
 pub trait WriteTrailer {
     /// Comment
-    fn write(
+    fn write_trailer(
         &self,
         writer: &mut impl Write,
         offset: usize,
         size: usize,
         root: ObjRef,
         id: [u8; 16],
-    ) -> Result<usize, std::io::Error>;
+    ) -> Result<(), std::io::Error>;
 }
 
 impl WriteTrailer for CrossReferenceTable {
-    fn write(
+    fn write_trailer(
         &self,
         writer: &mut impl Write,
         offset: usize,
         size: usize,
         root: ObjRef,
         id: [u8; 16],
-    ) -> Result<usize, std::io::Error> {
+    ) -> Result<(), std::io::Error> {
         const SIZE: Name = Name::new(b"Size");
         const ROOT: Name = Name::new(b"Root");
         const ID: Name = Name::new(b"ID");
@@ -41,7 +41,7 @@ impl WriteTrailer for CrossReferenceTable {
         const START_XREF_MARKER: &[u8] = b"startxref\n";
 
         let indent = &constants::SP.repeat(TRAILER_MARKER.len() - 1);
-        let written = types::write_chain! {
+        types::write_chain! {
             // dict start
             writer.write(TRAILER_MARKER),
             writer.write(indent),
@@ -70,6 +70,6 @@ impl WriteTrailer for CrossReferenceTable {
             writer.write(constants::NL_MARKER),
         };
 
-        Ok(written)
+        Ok(())
     }
 }
