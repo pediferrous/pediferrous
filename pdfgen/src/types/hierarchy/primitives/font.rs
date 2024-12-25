147fae1f8a112b28837966d9fbb4505b0e56b94f
Kasim Fejzic <kasimfejzo@gmail.com>
2024-12-25 01:09:07 +0100

    feat: implement `Object` trait for `Font` structure

--- pdfgen/src/types/hierarchy/primitives/font.rs
+++ pdfgen/src/types/hierarchy/primitives/font.rs
@@ -1,13 +1,17 @@
 //! Comment
 
-use crate::types;
+use crate::types::{self, constants};
 
-use super::{name::Name, object::Object};
+use super::{name::Name, obj_id::ObjId, object::Object};
 
 /// Comment
 pub struct Font<const N: usize> {
     /// Comment
+    id: ObjId,
+
+    /// Comment
     subtype: &'static [u8; N],
+
     /// Comment
     base_type: &'static [u8; N],
 }
@@ -16,4 +20,42 @@ impl<const N: usize> Font<N> {
     const FONT: Name = Name::new(b"Font");
     const SUBTYPE: Name = Name::new(b"Subtype");
     const BASE_FONT: Name = Name::new(b"BaseFont");
+
+    /// Comment
+    pub fn new(id: ObjId, subtype: &'static [u8; N], base_type: &'static [u8; N]) -> Self {
+        Font {
+            id,
+            subtype,
+            base_type,
+        }
+    }
+}
+
+impl<const N: usize> Object for Font<N> {
+    fn write(&self, writer: &mut impl std::io::Write) -> Result<usize, std::io::Error> {
+        let subtype_value: Name = Name::new(self.subtype);
+        let base_type_value: Name = Name::new(self.base_type);
+
+        let bytes_written = types::write_chain! {
+            writer.write(b"<< "),
+
+            // /Type /Font
+            Name::TYPE.write(writer),
+            Self::FONT.write(writer),
+            writer.write(constants::NL_MARKER),
+
+            // /Subtype /xyz
+            Self::SUBTYPE.write(writer),
+            subtype_value.write(writer),
+            writer.write(constants::NL_MARKER),
+
+            // /BaseFont /xyz
+            Self::BASE_FONT.write(writer),
+            base_type_value.write(writer),
+
+            writer.write(b">>"),
+        };
+
+        Ok(bytes_written)
+    }
 }
