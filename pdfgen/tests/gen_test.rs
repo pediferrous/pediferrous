101b33bff56de9bb021c4ea14420302ffd6a6d37
Nadir Fejzic <nadir@notfloor.com>
2024-12-28 14:47:13 +0100

    test: add simple tests for image loading

--- pdfgen/tests/gen_test.rs
+++ pdfgen/tests/gen_test.rs
@@ -1,4 +1,9 @@
-use pdfgen::{types::hierarchy::primitives::rectangle::Rectangle, Document};
+use std::path::PathBuf;
+
+use pdfgen::{
+    types::hierarchy::primitives::{rectangle::Rectangle, unit::Unit},
+    Document,
+};
 
 mod macros;
 
@@ -28,6 +33,23 @@ fn three_pages_different_size() {
     a5_page.set_mediabox(Rectangle::A5);
 
     document.create_page();
+
+    macros::snap_test!(document);
+}
+
+#[test]
+fn page_with_image() {
+    let mut document = Document::builder().with_page_size(Rectangle::A4).build();
+
+    let img = document
+        .load_image(PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("sample_image.jpg"))
+        .unwrap();
+
+    img.set_dimensions(Unit::from_mm(256.0), Unit::from_mm(256.0));
+
+    let img_ref = img.obj_ref().clone();
+    let page = document.create_page();
+    page.add_image(img_ref);
 
     macros::snap_test!(document);
 }
