ca4b66be511f4a5d1c04d21df2c4c1d13098c03d
Nadir Fejzic <nadir@notfloor.com>
2025-01-02 15:05:46 +0100

    docs: write documentation for `ContentStream`

--- pdfgen/src/types/hierarchy/content/content_stream.rs
+++ pdfgen/src/types/hierarchy/content/content_stream.rs
@@ -5,31 +5,51 @@ use crate::types::{
 
 use super::{image::ImageTransform, stream::Stream};
 
+/// Represents a specific operation in [`ContentStream`] such as drawing an image or text.
 pub(crate) enum Operation<'a> {
+    /// Represents a image drawing operation.
     DrawImage {
+        /// Name of the [`Image`] as defined in [`Resources`] of a [`Page`].
+        ///
+        /// [`Image`]: super::image::Image
+        /// [`Resources`]: crate::types::hierarchy::primitives::resources::Resources
+        /// [`Page`]: crate::types::hierarchy::page::Page
         name: &'a OwnedName,
+
+        /// Transformation that should be applied to the [`Image`] in Pdf.
+        ///
+        /// [`Image`]: super::image::Image
         transform: ImageTransform,
     },
 }
 
+/// Represents the content stream object that is used for encoding and rendering content of a
+/// [`Page`].
+///
+/// [`Page`]: crate::types::hierarchy::page::Page
 #[derive(Debug, PartialEq, PartialOrd)]
 pub struct ContentStream {
+    /// Inner stream object containing the actual bytes of the content.
     stream: Stream,
 }
 
 impl ContentStream {
+    /// Creates a new `ContentStream` with the given [`ObjId`].
     pub fn new(id: ObjId) -> Self {
         Self {
             stream: Stream::new(id),
         }
     }
 
+    /// Adds a content to this `ContentStream` that should be displayed on a [`Page`]. Content is
+    /// added in means of `Operation` that describes specific content elements.
     pub(crate) fn add_content(&mut self, operation: Operation) {
         match operation {
             Operation::DrawImage { name, transform } => self.draw_image(name, transform),
         }
     }
 
+    /// Encodes an image in this `ContentStream`.
     fn draw_image(&mut self, name: &OwnedName, transform: ImageTransform) {
         let Position {
             x: width,
