9296506095fe3e0645c2a1376d7b14cd3b6e0b7d
Kasim Fejzic <kasimfejzo@gmail.com>
2025-01-19 00:10:51 +0100

    docs: introduce const proc macro documentation

--- pdfgen_macros/src/lib.rs
+++ pdfgen_macros/src/lib.rs
@@ -25,7 +25,30 @@ impl Parse for ConstName {
     }
 }
 
-/// Comment
+/// Generate one or more `const Name<&'static [u8]>` values from the given identifiers. Identifiers
+/// should be specified in upper snake case and will be converted to pascal-cased PDF names. All
+/// leters will be replaced with their lowercase equivalents, except the first letter and any
+/// letter preceded by an underscore. For example:
+///
+/// * `IDENT` -> `Ident`
+/// * `SECOND_IDENT` -> `SecondIdent`
+///
+/// # Example
+///
+/// ```ignore
+/// use pdfgen_macros::const_names;
+///
+/// pub struct SomeStruct;
+///
+/// impl SomeStruct {
+/// const_names(TYPE, SUBTYPE, MEDIA_BOX);
+///
+/// // expands to
+/// const TYPE: Name<&'static [u8]> = Name::from_static(b"Type");
+/// const SUBTYPE: Name<&'static [u8]> = Name::from_static(b"Subtype");
+/// const MEDIA_BOX: Name<&'static [u8]> = Name::from_static(b"MediaBox");
+/// }
+/// ```
 #[proc_macro]
 pub fn const_names(token_stream: TokenStream) -> TokenStream {
     let const_name =
@@ -49,7 +72,7 @@ pub fn const_names(token_stream: TokenStream) -> TokenStream {
     ts
 }
 
-/// Comment
+/// Helper function converting uppercase literals to LitByteStr in PascalCase format
 fn create_pdf_style_byte_literal(name: &Ident) -> LitByteStr {
     let name = name.to_string();
 
