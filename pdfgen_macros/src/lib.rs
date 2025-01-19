6e853c5cd744ffa55cec33a8fab294ddad6fb84b
Nadir Fejzic <nadir@notfloor.com>
2025-01-19 14:24:36 +0100

    docs: move `const_names` docs to the correct function

--- pdfgen_macros/src/lib.rs
+++ pdfgen_macros/src/lib.rs
@@ -2,6 +2,30 @@ use proc_macro::TokenStream;
 
 mod name;
 
+/// Generate one or more `const Name<&'static [u8]>` values from the given identifiers. Identifiers
+/// should be specified in upper snake case and will be converted to pascal-cased PDF names. All
+/// leters will be replaced with their lowercase equivalents, except the first letter and any
+/// letter preceded by an underscore. For example:
+///
+/// * `IDENT` -> `Ident`
+/// * `SECOND_IDENT` -> `SecondIdent`
+///
+/// # Example
+///
+/// ```ignore
+/// use pdfgen_macros::const_names;
+///
+/// pub struct SomeStruct;
+///
+/// impl SomeStruct {
+///     const_names!(TYPE, SUBTYPE, MEDIA_BOX);
+///
+///     // expands to
+///     const TYPE: Name<&'static [u8]> = Name::from_static(b"Type");
+///     const SUBTYPE: Name<&'static [u8]> = Name::from_static(b"Subtype");
+///     const MEDIA_BOX: Name<&'static [u8]> = Name::from_static(b"MediaBox");
+/// }
+/// ```
 #[proc_macro]
 pub fn const_names(token_stream: TokenStream) -> TokenStream {
     name::const_names(token_stream)
--- pdfgen_macros/src/name.rs
+++ pdfgen_macros/src/name.rs
@@ -33,30 +33,6 @@ impl Parse for ConstName {
     }
 }
 
-/// Generate one or more `const Name<&'static [u8]>` values from the given identifiers. Identifiers
-/// should be specified in upper snake case and will be converted to pascal-cased PDF names. All
-/// leters will be replaced with their lowercase equivalents, except the first letter and any
-/// letter preceded by an underscore. For example:
-///
-/// * `IDENT` -> `Ident`
-/// * `SECOND_IDENT` -> `SecondIdent`
-///
-/// # Example
-///
-/// ```ignore
-/// use pdfgen_macros::const_names;
-///
-/// pub struct SomeStruct;
-///
-/// impl SomeStruct {
-///     const_names!(TYPE, SUBTYPE, MEDIA_BOX);
-///
-///     // expands to
-///     const TYPE: Name<&'static [u8]> = Name::from_static(b"Type");
-///     const SUBTYPE: Name<&'static [u8]> = Name::from_static(b"Subtype");
-///     const MEDIA_BOX: Name<&'static [u8]> = Name::from_static(b"MediaBox");
-/// }
-/// ```
 pub fn const_names(token_stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let const_name =
         parse_macro_input!(token_stream with Punctuated<ConstName, Token![,]>::parse_terminated);
