4d47ffc5ea3294415d8bd4d4883fe4b4cac2b852
Kasim Fejzic <kasimfejzo@gmail.com>
2025-03-05 22:00:18 +0100

    feat: create a rough version of the Text struct

--- pdfgen/src/types/hierarchy/primitives/text.rs
+++ pdfgen/src/types/hierarchy/primitives/text.rs
@@ -1,1 +1,33 @@
 //! Implementation of PDF Text object.
+
+use super::{font::Font, object::Object, rectangle::Position, string::PdfString};
+
+/// Comment
+#[derive(Debug)]
+pub struct TextTransform {
+    /// Comment
+    position: Position,
+
+    /// Comment
+    size: u32,
+}
+
+/// Comment
+#[derive(Debug)]
+pub struct Text {
+    /// Comment
+    content: PdfString,
+
+    /// Comment
+    transform: TextTransform,
+
+    /// Comment
+    font: Font,
+}
+
+impl Text {
+    /// Comment
+    fn new(content: &str) -> Self {
+        Self { content: content, transform TextTransform::default(), font: Font::default() }
+    }
+}
