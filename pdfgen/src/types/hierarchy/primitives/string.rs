be358e59834d2767b257d95956bde4f8d0af1bd8
Kasim Fejzic <kasimfejzo@gmail.com>
2025-03-05 22:34:44 +0100

    feat: adapt `PdfString` implementation to suit the PDF-s `Text` object

--- pdfgen/src/types/hierarchy/primitives/string.rs
+++ pdfgen/src/types/hierarchy/primitives/string.rs
@@ -1,62 +1,63 @@
 use std::io::{Error, Write};
 
-use crate::{
-    types::{constants, hierarchy::content::stream::Stream},
-    ObjId,
-};
-
-use super::object::Object;
+use pdfgen_macros::write_chain;
 
 /// Represents a PDF String with UTF-8 encoding.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PdfString {
-    id: ObjId,
-
-    /// Inner [`Stream`] is the object that actually stores the bytes of a `PdfString`.
-    stream: Stream,
+    /// Inner [`String`] is the object that actually stores the bytes of a `PdfString`.
+    inner: String,
 }
 
 impl PdfString {
-    /// Creates a new `PdfString` with the given value that can be converted to UTF-8 encoded
+    /// Creates a new `PdfString` with the given content that can be converted to UTF-8 encoded
     /// [`String`].
-    pub fn from(id: ObjId, string: impl Into<String>) -> Self {
-        // NOTE: For text strings encoded in UTF-8, the first three bytes shall be 239 followed by
-        //       187, followed by 191.
-        let mut bytes = vec![b'(', 239, 187, 191];
-
-        bytes.extend(string.into().into_bytes());
-        bytes.push(b')');
-
+    pub fn from(content: impl Into<String>) -> Self {
         Self {
-            id,
-            stream: Stream::with_bytes(bytes),
+            inner: content.into(),
         }
     }
-}
 
-impl Object for PdfString {
-    fn write_def(&self, writer: &mut dyn std::io::Write) -> Result<usize, std::io::Error> {
-        Ok(pdfgen_macros::write_chain! {
-            self.id.write_def(writer),
-            writer.write(constants::NL_MARKER),
-        })
+    /// Expands the `PdfString` with the given content that can be converted to UTF-8 encoded
+    /// [`String`].
+    pub fn expand(&mut self, content: impl Into<String>) {
+        self.inner.push_str(&content.into())
     }
 
-    fn write_content(&self, writer: &mut dyn Write) -> Result<usize, Error> {
-        self.stream.write(writer)
+    /// Writes the inner content in the PDF String syntax format to the provided writer.
+    pub fn write_content(&self, writer: &mut dyn Write) -> Result<usize, Error> {
+        Ok(write_chain! {
+            writer.write(b"("),
+            writer.write(self.inner.as_bytes()),
+            writer.write(b")"),
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{types::hierarchy::primitives::object::Object, IdManager};
-
     use super::PdfString;
 
     #[test]
     fn simple_string() {
-        let mut id_manager = IdManager::new();
-        let pdf_string = PdfString::from(id_manager.create_id(), "This is text.");
+        let pdf_string = PdfString::from("This is text.");
+
+        let mut writer = Vec::default();
+        pdf_string.write_content(&mut writer).unwrap();
+        let output = String::from_utf8(writer).unwrap();
+
+        insta::assert_snapshot!(output, @r"
+        << /Length 18 >>
+        stream
+        (﻿This is text.)
+        endstream
+        ");
+    }
+
+    #[test]
+    fn simple_string_expanded() {
+        let mut pdf_string = PdfString::from("This is");
+        pdf_string.expand(" an expanded text.");
 
         let mut writer = Vec::default();
         pdf_string.write_content(&mut writer).unwrap();
