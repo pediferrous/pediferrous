d03261c3830de22316e283b555e8f840725f4d9c
Kasim Fejzic <kasimfejzo@gmail.com>
2025-03-10 01:33:26 +0100

    feat: implement `TexBuilder` support for `Text` object

--- pdfgen/src/types/hierarchy/primitives/text.rs
+++ pdfgen/src/types/hierarchy/primitives/text.rs
@@ -1,10 +1,12 @@
 //! Implementation of PDF Text object.
 
-use super::{font::Font, object::Object, rectangle::Position, string::PdfString};
+use crate::types::constants;
+
+use super::{object::Object, rectangle::Position, string::PdfString};
 
 /// Comment
 #[derive(Debug)]
-pub struct TextTransform {
+pub(crate) struct TextTransform {
     /// Comment
     position: Position,
 
@@ -22,13 +24,40 @@ pub struct Text {
     transform: TextTransform,
 
     /// Comment
-    font: Font,
+    font_name: String,
 }
 
 impl Text {
     /// Comment
-    fn new(content: &str) -> Self {
-        Self { content: content, transform TextTransform::default(), font: Font::default() }
+    pub const BT_MARKER: &[u8] = b"BT";
+    /// Comment
+    pub const ET_MARKER: &[u8] = b"ET";
+    /// Comment
+    pub const TF_OPERATOR: &[u8] = b"Tf";
+    /// Comment
+    pub const TD_OPERATOR: &[u8] = b"Td";
+    /// Comment
+    pub const TJ_OPERATOR: &[u8] = b"Tj";
+
+    /// Comment
+    fn builder() -> TextBuilder<false> {
+        let txt = Self {
+            content: PdfString::from(""),
+            transform: TextTransform {
+                position: Position::from_mm(0.0, 0.0),
+                size: 12,
+            },
+            font_name: String::from_utf8_lossy(constants::DEFAULT_FONT).to_string(),
+        };
+
+        TextBuilder { inner: txt }
+    }
+
+    /// Comment
+    fn expand(&mut self, content: impl Into<String>) {
+        self.content.expand(content);
+    }
+}
     }
 }
 /// Comment
@@ -44,14 +73,33 @@ impl<const IS_INIT: bool> TextBuilder<IS_INIT> {
         TextBuilder { inner: self.inner }
     }
 
-    /// Sets the scaling of the image to the given width and height.
+    /// Sets the content of the [`Text`].
+    pub fn with_content(mut self, content: impl Into<String>) -> Self {
+        self.inner.content = PdfString::from(content);
+        self
+    }
+
+    /// Expands the content of the [`Text`] with the provided content.
+    pub fn with_expanded_content(mut self, content: impl Into<String>) -> Self {
+        self.inner.expand(content);
+        self
+    }
+
+    /// Sets the size of the [`Text`].
     pub fn with_size(mut self, size: u32) -> Self {
         self.inner.transform.size = size;
+        self
+    }
+
+    /// Sets the font of the [`Text`].
+    pub fn with_font(mut self, font_name: impl Into<String>) -> Self {
+        self.inner.font_name = font_name.into();
         self
     }
 }
 
 impl TextBuilder<true> {
+    /// Comment
     pub fn build(self) -> Text {
         self.inner
     }
