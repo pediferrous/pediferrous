e0bcb689ada526810770408ea025648ede72ef43
Nadir Fejzic <nadir@notfloor.com>
2025-07-25 02:34:51 +0200

    docs: update docs on `CmykValue`

--- pdfgen/src/types/hierarchy/content/color/cmyk_value.rs
+++ pdfgen/src/types/hierarchy/content/color/cmyk_value.rs
@@ -1,15 +1,22 @@
+//! Implementation of PDF Color Space and Color specification.
+
 use std::fmt;
 
+/// Possible errors that might be returned when creating a new [`CmykValue`] instance.
 #[derive(Debug, thiserror::Error)]
 pub enum CmykValueErr<T: fmt::Display> {
+    /// Indicates that the provided does not fit into [`CmykValue`].
     #[error("Provided value '{}' is out of range", .0)]
     OutOfRange(T),
 }
 
+/// Newtype for ensuring correct values are used in CMYK color space.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CmykValue(u8);
 
 impl CmykValue {
+    /// Create a new [`CmykValue`] from a constant that is in range. Range is checked at compile
+    /// time.
     pub const fn from_const<const N: u8>() -> Self {
         if N > 100 {
             panic!("Only values in range [0, 100] are allowed.");
@@ -22,6 +29,7 @@ impl CmykValue {
 impl TryFrom<u8> for CmykValue {
     type Error = CmykValueErr<u8>;
 
+    /// Create a new [`CmykValue`] from an [`u8`], with valid range being `[0, 100]`.
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         if value > 100 {
             return Err(CmykValueErr::OutOfRange(value));
@@ -34,6 +42,7 @@ impl TryFrom<u8> for CmykValue {
 impl TryFrom<f32> for CmykValue {
     type Error = CmykValueErr<f32>;
 
+    /// Create a new [`CmykValue`] from an [`f32`], with valid range being `[0.0, 1.0]`.
     fn try_from(value: f32) -> Result<Self, Self::Error> {
         if !(0. ..=1.).contains(&value) {
             return Err(CmykValueErr::OutOfRange(value));
